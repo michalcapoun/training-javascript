a8f3d07741c4bf9053845a9ca2cd601b20b0f225
Michal Čapoun <michal.capoun@gmail.com>
2023-08-31 11:48:58 +0200

    Create wheatFromChaff.test.js

---
+++ wheatFromChaff.test.js
@@ -0,0 +1,10 @@
+const {wheatFromChaff} = require('./wheatFromChaff');
+
+describe('wheatFromChaff', function () {
+    it('Check medium length array', function () {
+        const testData = [-31, -5, 11, -42, -22, -46, -4, -28];
+        const expected = [-31, -5, -28, -42, -22, -46, -4, 11];
+
+        expect(wheatFromChaff(testData)).toEqual(expected);
+    });
+});
