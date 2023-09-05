d83778f4fcc0dda47c046d8018beff4642201c25
Michal Čapoun <michal.capoun@gmail.com>
2023-09-05 09:09:40 +0200

    refactor pigLatin

--- tests/pigLatin.test.js
+++ tests/pigLatin.test.js
@@ -1,14 +1,14 @@
-const {pigLatin} = require('../src/simplePigLatin');
+const {pigLatin} = require('../src/pigLatin');
 
 const testData1 = 'Pig latin is cool';
 const testData2 = 'Hello world !';
 
 describe('pigLatin', function () {
-    it(`'${testData1}' should return correct output`, function () {
+    it(`'${testData1}' should test simple text`, function () {
         expect(pigLatin(testData1)).toEqual('igPay atinlay siay oolcay');
     });
 
-    it(`'${testData2}' should return correct output`, function () {
+    it(`'${testData2}' should test special characters`, function () {
         expect(pigLatin(testData2)).toEqual('elloHay orldway !');
     });
 });
