69be9bf697f596003a06ff7120dbbfd9a646bfad
Michal Čapoun <michal.capoun@gmail.com>
2023-09-10 17:12:45 +0200

    Create longestRepetition.js

---
+++ src/longestRepetition.js
@@ -0,0 +1,32 @@
+// For a given string s find the character c (or C) with longest consecutive repetition and return:
+// [c, l]
+// where l (or L) is the length of the repetition. If there are two or more characters with the same l return the first in order of appearance.
+// For empty string return:
+// ["", 0]
+
+function longestRepetition(str) {
+    if (str === "") return ['', 0];
+
+    let maxChar = '';
+    let maxLength = 0;
+    let currentChar = '';
+    let currentLength = 0;
+
+    for (let index = 0; index < str.length; index++) {
+        if (str[index] === currentChar) {
+            currentLength++;
+        } else {
+            currentChar = str[index];
+            currentLength = 1;
+        }
+
+        if (currentLength > maxLength) {
+            maxLength = currentLength;
+            maxChar = currentChar;
+        }
+    }
+
+    return [maxChar, maxLength];
+}
+
+module.exports = {longestRepetition};
