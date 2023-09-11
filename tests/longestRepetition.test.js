d97d50f9e227b902a170f7e167215df4ecd8f365
Michal Čapoun <michal.capoun@gmail.com>
2023-09-11 06:58:08 +0200

    remove semicolons

--- tests/longestRepetition.test.js
+++ tests/longestRepetition.test.js
@@ -1,11 +1,11 @@
-const {longestRepetition} = require('../src/longestRepetition');
+const {longestRepetition} = require('../src/longestRepetition')
 
 describe('longestRepetition', () => {
     it('should test normal string', () => {
         expect(longestRepetition("aaaabb")).toEqual(['a', 4])
-    });
+    })
 
     it('should test empty string', () => {
         expect(longestRepetition("")).toEqual(['', 0])
-    });
-});
+    })
+})
