e3128778b9e06ced3fba255617843711422aa85a
Michal Čapoun <michal.capoun@gmail.com>
2023-09-12 10:34:10 +0200

    Create areTheySame.js

---
+++ src/areTheySame.js
@@ -0,0 +1,24 @@
+// Given two arrays a and b write a function comp(a, b) (orcompSame(a, b)) that checks whether the two arrays have the "same" elements, with the same multiplicities (the multiplicity of a member is the number of times it appears). "Same" means, here, that the elements in b are the elements in a squared, regardless of the order.
+// Examples
+// Valid arrays
+// a = [121, 144, 19, 161, 19, 144, 19, 11]
+// b = [121, 14641, 20736, 361, 25921, 361, 20736, 361]
+// comp(a, b) returns true because in b 121 is the square of 11, 14641 is the square of 121, 20736 the square of 144, 361 the square of 19, 25921 the square of 161, and so on. It gets obvious if we write b's elements in terms of squares:
+// a = [121, 144, 19, 161, 19, 144, 19, 11]
+// b = [11*11, 121*121, 144*144, 19*19, 161*161, 19*19, 144*144, 19*19]
+
+function comp(arrA, arrB) {
+    if (!arrA || !arrB || arrA.length !== arrB.length) return false;
+
+    let arrASquared = arrA.map(x => x * x).sort((x, y) => x - y);
+    let arrBSorted = arrB.slice().sort((x, y) => x - y);
+
+
+    for (let index = 0; index < arrA.length; index++) {
+        if (arrASquared[index] !== arrBSorted[index]) return false;
+    }
+
+    return true;
+}
+
+module.exports = {comp};
